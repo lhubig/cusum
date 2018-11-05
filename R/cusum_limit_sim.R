5b5bb1a85685ebfa7abf788660acc7e3564a4a46
Lena Hubig <hubig@ibe.med.uni-muenchen.de>
2018-11-05 14:30:38 +0100

    changed names

---
+++ R/cusum_limit_sim.R
@@ -0,0 +1,72 @@
+#' Simulate control limit given false signal probability alpha for CUSUM charts
+#'
+#' @export
+#' @import checkmate
+#' @import stats
+#' @param failure_probability Accepted failure probability of process
+#' @param n_patients Sample size
+#' @param odds_multiplier Odds multiplier for the alternative hypothesis (<1 looks for decreases)
+#' @param n_simulation Number of simulations
+#' @param alpha False signal probability
+#' @param seed Add an optional seed for simulation
+#' @return Returns the CL
+#' @examples
+#'
+#' # simulate control limits for alpha = 0.05
+#' cusum_limit_sim(failure_probability = 0.05,
+#'     n_patients = 100,
+#'     odds_multiplier = 2,
+#'     n_simulation = 1000,
+#'     alpha = 0.05,
+#'     seed = 2046)
+
+
+cusum_limit_sim <- function(failure_probability, n_patients, odds_multiplier, n_simulation, alpha, seed = NULL) {
+
+  ## Check user input ####
+  assert_numeric(failure_probability, lower = 0, upper = 1, finite = TRUE, any.missing = FALSE, len = 1)
+  if (failure_probability > 0.5) {
+    failure_probability <- 1 - failure_probability
+    warning("Accepted failure probability failure_probability will be recoded to 1-failure_probability when > 0.5.")
+  }
+
+  n <- as.integer(n_patients)
+  assert_integer(n, lower = 1, any.missing = FALSE, len = 1)
+
+  assert_numeric(odds_multiplier, lower = 0, len = 1, finite = TRUE, any.missing = FALSE)
+  if (odds_multiplier < 1) {
+    message("CUSUM detects process improvements (odds_multiplier < 1). ")
+  }
+  if (odds_multiplier == 1) {
+    warning("CUSUM detects no process change (odds_multiplier = 1).")
+  }
+
+  assert_numeric(n_simulation, lower = 1, len = 1, finite = TRUE, any.missing = FALSE)
+
+  assert_numeric(alpha, lower = 0, len = 1, finite = TRUE, any.missing = FALSE)
+
+  assert_numeric(as.numeric(seed), lower = 0, max.len = 1)
+
+  ## Simulate CUSUM runs ####
+  cs_sim <- function(i, npat = n, p = failure_probability, or = odds_multiplier) {
+    p.0 <- p
+    o.0 <- p.0 / (1 - p.0)
+    o.1 <- o.0 * or
+    p.1 <- o.1 / (1 + 0.1)
+
+    y <- rbinom(npat, 1, p.0)
+    w.t <- y * log(p.1 / p.0) + (1 - y) * log((1 - p.1) / (1 - p.0))
+    c.t <- vector(mode = "numeric", length = npat)
+    c.t[1] <- max(c(0, c.t[1] + w.t[1]))
+    for (i in 2:npat) c.t[i] <- max(c(0, c.t[i - 1] + w.t[i]))
+    return(max(c.t))
+  }
+
+  set.seed(seed)
+  rl <- lapply(1:n_simulation, cs_sim)
+
+  ## Estimate Alpha ####
+  cl <- quantile(unlist(rl), 1 - alpha)
+
+  return(as.numeric(cl))
+}
---
+++ R/racusum_limit_sim.R
@@ -0,0 +1,89 @@
+#' Simulate control limit given false signal probability alpha for RA-CUSUM charts
+#'
+#' False-signal-probability-simulation of Control Limits h for risk-adjusted CUSUM charts
+#' @export
+#' @import checkmate
+#' @import stats
+#' @param patient_risks Vector of failure probabilities
+#' @param n_simulation Number of simulations
+#' @param odds_multiplier Odds multiplier for the alternative hypothesis (<1 looks for decreases)
+#' @param alpha False signal probability
+#' @param seed Add an optional seed for simulation
+#' @return Returns the control limit
+#' @examples
+#'
+#' # Patients risks are usually known from Phase I.
+#' # If not, these risk scores can be simulated.
+#'
+#' # define possible patient risk scores
+#' risks <- c(0.001,0.01,0.1,0.002,0.02,0.2)
+#'
+#' # sample risk population of size n = 100
+#' set.seed(2046)
+#' patient_risks <- sample(x = risks, size = 100, replace = TRUE)
+#'
+#' # simulate control limits for alpha = 0.05
+#' racusum_limit_sim(patient_risks,
+#'    odds_multiplier = 2,
+#'    n_simulation = 1000,
+#'    alpha = 0.05,
+#'    seed = 2046)
+
+racusum_limit_sim <- function(patient_risks, odds_multiplier, n_simulation, alpha, seed = NULL) {
+
+  ## Check user input ####
+  assert_numeric(patient_risks, lower = 0, upper = 1, min.len = 1, finite = TRUE, any.missing = FALSE)
+
+  assert_numeric(odds_multiplier, lower = 0, len = 1, finite = TRUE, any.missing = FALSE)
+  if (odds_multiplier < 1) {
+    message("CUSUM detects process improvements (odds_multiplier < 1). ")
+  }
+  if (odds_multiplier == 1) {
+    warning("CUSUM detects no process change (odds_multiplier = 1).")
+  }
+
+  assert_numeric(n_simulation, lower = 1, len = 1, finite = TRUE, any.missing = FALSE)
+
+  assert_numeric(alpha, lower = 0, len = 1, finite = TRUE, any.missing = FALSE)
+
+  assert_numeric(as.numeric(seed), lower = 0, max.len = 1)
+
+  ## Calculate risk distribution ####
+  n <- length(patient_risks)
+
+  freq_score <- table(patient_risks)
+  freq_score <- as.data.frame(freq_score)
+
+
+  freq <- round(as.numeric(freq_score$Freq / sum(freq_score$Freq)), digits = 1000)
+  risk <- sort(unique(patient_risks)) # corresponding risk scores
+
+
+  if (sum(freq) != 1) {
+    warning("Sum of risk frequence != 1.")
+  }
+
+  ## Simulate CUSUM runs ####
+  cs_sim <- function(i) {
+    p.0 <- sample(risk, size = n, replace = T, prob = freq)
+
+
+    y <- rbinom(n, 1, p.0)
+    ws <- log(1 / (1 + (odds_multiplier - 1) * p.0)) # success (non death) case
+    wf <- log(odds_multiplier / (1 + (odds_multiplier - 1) * p.0)) # failure (death) case
+
+    w.t <- ifelse(y == 1, wf, ws)
+    c.t <- vector(mode = "numeric", length = n)
+    c.t[1] <- max(c(0, c.t[1] + w.t[1]))
+    for (i in 2:n) c.t[i] <- max(c(0, c.t[i - 1] + w.t[i]))
+    return(max(c.t))
+  }
+
+  set.seed(seed)
+  rl <- lapply(1:n_simulation, cs_sim)
+
+  ## Estimate Alpha ####
+  q <- quantile(unlist(rl), 1 - alpha)
+
+  return(as.numeric(q))
+}
