a2ecbc127249c3528bbf3fcd39d27f02dfc30c67
Lena Hubig <hubig@ibe.med.uni-muenchen.de>
2019-08-13 14:50:17 +0200

    grouped cusum example data

--- R/data.R
+++ R/data.R
@@ -33,3 +33,22 @@
 #'
 #' Description of performance indicator (in German): \url{https://iqtig.org/downloads/auswertung/2016/10n2karot/QSKH_10n2-KAROT_2016_QIDB_V02_2017-04-26.pdf}
 "racusum_example_data"
+
+#' Grouped Non-Risk-adjusted Performance Data with Block Identifier
+#'
+#' Generated performance data of indicator 17/1 54030: Preoperative stay over 24 hours for patients with proximal femur fracture. 
+#' 
+#'
+#' Patient outcomes were simulated based on average national failure rate. Two years are provided, so Phase I and Phase II can be defined.
+#'
+#' @format A data frame with 2000 rows and 4 variables:
+#' \describe{
+#'   \item{t}{Sequence of observations}
+#'   \item{y}{Patient outcome}
+#'   \item{year}{Year of treatment}
+#'   \item{block_identifier}{Continuous block identifier}
+#' }
+#' @source Data for simulation was provided by Bavarian Agency of Quality Assurance (BAQ), Munich Germany.
+#'
+#' Description of performance indicator (in German): \url{https://iqtig.org/downloads/auswertung/2016/17n1hftfrak/QSKH_17n1-HUEFTFRAK_2016_QIDB_V02_2017-04-26.pdf}
+"gcusum_example_data"
