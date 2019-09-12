2d9578054bb22c26fe60878f5765a22e64b4d6d6
Lena Hubig <hubig@ibe.med.uni-muenchen.de>
2019-09-12 11:46:36 +0200

    expect class of cusum

--- tests/testthat/test-cusum.R
+++ tests/testthat/test-cusum.R
@@ -3,7 +3,7 @@ context("cusum")
 set.seed(1500)
 outcomes <- as.logical(rbinom(n = 10, size = 1, prob = 0.05))
 
-test_that("Output of Control limit simulation", {
+test_that("Output of CUSUM", {
   expected_results <- c(0.00, 0.64, 0.60, 0.55, 0.50, 0.45, 0.40, 1.04, 1.00, 0.95)
   works <- cusum(
     failure_probability = 0.05,
@@ -14,4 +14,26 @@ test_that("Output of Control limit simulation", {
   )
   result <- round(works$ct, 2)
   expect_equal(result, expected_results)
+}
+)
+
+test_that("Number of patients", {
+ 
+    x <- cusum(failure_probability = 0.1,
+               patient_outcomes = rbinom(n = 100, size = 1, prob = .1),
+               limit = 2.5,
+               odds_multiplier = 2,
+               reset = FALSE)
+
+    expect_equal(nrow(x), 100)
+    }
+)
+
+test_that("Class of cusum", {
+  expect_class(cusum(failure_probability = 0.1,
+                     patient_outcomes = rbinom(n = 100, size = 1, prob = .1),
+                     limit = 2.5,
+                     odds_multiplier = 2,
+                     reset = FALSE), c("cusum", "data.frame"))
+  
 })
