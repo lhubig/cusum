89c174f7e064163aacf01293f6882e9de2af562f
Lena Hubig <hubig@ibe.med.uni-muenchen.de>
2019-09-12 11:55:21 +0200

    test results of cusum_limit_exact

---
+++ tests/testthat/test_cusum_limit_exact.R
@@ -0,0 +1,11 @@
+context("cusum")
+
+test_that("Exact calculation of CL",{
+  x <- unname(round(cusum_limit_exact(failure_probability = 0.1,
+                               n_patients  = 10,
+                               odds_multiplier = 2,
+                               alpha = 0.05),5))
+  expect_equal(x,
+               1.41227)
+  }
+  )
