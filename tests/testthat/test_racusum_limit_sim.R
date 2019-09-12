69ca0482a566337aa3d3217b99ea03f0df5061b7
Lena Hubig <hubig@ibe.med.uni-muenchen.de>
2019-09-12 13:17:58 +0200

    test racusum_limit_sim output

--- tests/testthat/test_racusum_limit_sim.R
+++ tests/testthat/test_racusum_limit_sim.R
@@ -1,20 +1,20 @@
 context("racusum_limit_sim")
 
-# risks <- c(0.001, 0.01, 0.1, 0.002, 0.02, 0.2)
-# 
-# set.seed(2046)
-# patient_risks <- sample(x = risks, size = 100, replace = TRUE)
-# 
-# test_that("Output of Control limit simulation", {
-#   expected_results <- 2.59
-#   works <- round(
-#     racusum_limit_sim(patient_risks,
-#       odds_multiplier = 2,
-#       n_simulation = 1000,
-#       alpha = 0.05,
-#       seed = 2046
-#     ),
-#     2
-#   )
-#   expect_equal(works, expected_results)
-# })
+risks <- c(0.001, 0.01, 0.1, 0.002, 0.02, 0.2)
+
+set.seed(2046)
+patient_risks <- sample(x = risks, size = 100, replace = TRUE)
+
+test_that("Output of Control limit simulation", {
+  expected_results <- 2.59
+  works <- round(
+    racusum_limit_sim(patient_risks,
+      odds_multiplier = 2,
+      n_simulation = 1000,
+      alpha = 0.05,
+      seed = 2046
+    ),
+    2
+  )
+  expect_equal(works, expected_results)
+})
