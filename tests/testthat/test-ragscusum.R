2de6db4b255571b0e93da9006f9fe9df715282d8
Lena Hubig <hubig@ibe.med.uni-muenchen.de>
2019-09-12 15:10:24 +0200

    test for ragscusum

---
+++ tests/testthat/test-ragscusum.R
@@ -0,0 +1,27 @@
+context("ragscusum")
+
+test_that("RAGSCUSUM results", {
+  set.seed(201511)
+  y <- rbinom(10,1,.2)
+  weight_f <- c(rep(0.5,5), rep(0.6,5))
+  weight_s <- c(rep(-.2,5), rep(-.1,5))
+  blocks <- c(1,1,1,2,2,3,3,3,3,3)
+  outcomes <- cbind(y, weight_f, weight_s, blocks)
+  
+  observed <- ragscusum(input_ra_outcomes = outcomes,
+                      limit = 3,
+                      quantiles = (0.5),
+                      seed = 2910)
+  exp_sig <- rep(0, 10)
+  expect_equal(observed[,1], exp_sig)
+  
+  exp_mean <- c(0,0,0,0,0,0.12,0.22,0.30,.36,.4)
+  expect_equal(round(observed[,2],2), exp_mean)
+  
+  exp_med <- c(0,0,0,0,0,0,0,0.4,0.4,0.4)
+  expect_equal(round(observed[,3],2), exp_med)
+  
+  
+})
+
+
