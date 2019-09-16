0575edce2a1f9f25a2754afaeb07ce32d60aeceb
Lena Hubig <hubig@ibe.med.uni-muenchen.de>
2019-09-16 15:35:57 +0200

    forgot curly brackets

--- tests/testthat/test-racusum_dpcl_sim.R
+++ tests/testthat/test-racusum_dpcl_sim.R
@@ -5,7 +5,7 @@ risks <- c(0.001, 0.01, 0.1, 0.002, 0.02, 0.2)
 set.seed(2046)
 patient_risks <- sample(x = risks, size = 10, replace = TRUE)
 
-test_that("Output of dpcl sim", 
+test_that("Output of dpcl sim", {
           observe <- round(dpcl <- racusum_limit_dpcl(
             patient_risks = patient_risks,
             N = 1000,
@@ -13,6 +13,8 @@ test_that("Output of dpcl sim",
             alpha = 0.05,
             seed = 32423
             ),4)
+          
           expect <- c(0,0,0,0,0,0,0.5108,0.5108,0.8393,0.5098)
           expect_equal(observe, expect)
+}          
 )
